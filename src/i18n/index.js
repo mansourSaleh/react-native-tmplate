e8dccce9a273f6fc96c2967895b26e43ad0906a7
mansourSaleh <mansouralmohsen2018@gmai.com>
2021-03-20 19:20:28 +0300

    edit i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,22 +2,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import {I18nManager} from 'react-native'
+import resources from './resources'
 
 
 
-const resources = {
-    en: {
-      translation: {
-        "Welcome to React": "Welcome to React and react-i18next",
-        "hello_word": "Hi"
-      }
-    },
-    ar: {
-        translation: {
-            "hello_word": "سلام"
-        }
-    }
-  };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
---
+++ src/i18n/resources.js
@@ -0,0 +1,15 @@
+const resources = {
+    en: {
+      translation: {
+        "Welcome to React": "Welcome to React and react-i18next",
+        "hello_word": "Hi"
+      }
+    },
+    ar: {
+        translation: {
+            "hello_word": "سلام"
+        }
+    }
+  };
+
+  export default resources;
